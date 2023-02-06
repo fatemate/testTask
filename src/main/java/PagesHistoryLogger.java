36b3600a16a1b51994d2c5a8cdca2c14595d1b9b
fatemate <Andry-NS@yandex.ru>
2023-02-06 20:35:24 +0700

    Adding, deleting elements and circular buffer indexing were added.

--- src/main/java/PagesHistoryLogger.java
+++ src/main/java/PagesHistoryLogger.java
@@ -8,6 +8,7 @@ public class PagesHistoryLogger {
     private Calendar calendar;
     private HashMap<String, String>[] maps = new HashMap[historyDaysDeep];
 
+
     /**
      *   The default constructor uses a TimeZone based on the time zone where the program is running.
      * */
@@ -21,12 +22,22 @@ public class PagesHistoryLogger {
         this.calendar = Calendar.getInstance(timezone);
     }
 
-    public void addPage(String url, String html) {
 
+    /**
+     * Adds a new page or replaces a page with the given key (url).
+     */
+    public void addPage(String url, String html) {
+        updateCalendar();
+        getHistoryDaysBefore(0).put(url, html);
     }
 
-    public void removePage(String url) {
 
+    /**
+     * Removes the page for the given key (url) if present.
+     */
+    public void removePage(String url) {
+        updateCalendar();
+        getHistoryDaysBefore(0).remove(url);
     }
 
     public ArrayList<String> getDifferencePages(int dayBeforeA, int dayBeforeB) {
@@ -61,15 +72,33 @@ public class PagesHistoryLogger {
         return null;
     }
 
+
+    /**
+     *   Returns a HashMap that contains the condition of pages for certain day until today.
+     *   If the argument is 0, than function will return the condition of pages for today.
+     *   The argument must be between 0 and historyDaysDeep otherwise the function will throw an IllegalArgumentException.
+     * */
     private HashMap<String, String> getHistoryDaysBefore(int numDaysBefore) {
         return maps[getIndexHistoryDaysBefore(numDaysBefore)];
     }
 
+
+    /**
+     *   Returns the index in a circular buffer (holding the page states for a specific day), corresponding to a specific day up to today.
+     *   If the argument is 0, then the function will return the index of the element corresponding to today.
+     *   The argument must be between 0 and historyDaysDeep otherwise the function will throw an IllegalArgumentException.
+     * */
     private int getIndexHistoryDaysBefore(int numDaysBefore) {
-        return 0;
+        if (numDaysBefore > historyDaysDeep || numDaysBefore < 0)
+            throw new IllegalArgumentException("Out of history deep range!\n"
+                    + "HISTORY_DEEP range: [ 0; "
+                    + historyDaysDeep
+                    + "], argument: "
+                    + numDaysBefore);
+        return (((calendar.get(Calendar.DAY_OF_YEAR) % historyDaysDeep) - numDaysBefore) + historyDaysDeep) % historyDaysDeep;
     }
 
     private void updateCalendar() {
-
+        calendar.setTimeInMillis(System.currentTimeMillis());
     }
 }
